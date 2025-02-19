5d5e43105029276359b7e3eea9f9694783cd2d0b
eubrunors <bruno.2013.br11@gmail.com>
2025-02-19 09:16:13 -0300

    :construction: feat(aluno): Add complete constructor and improve initialization

--- src/model/Aluno.java
+++ src/model/Aluno.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Aluno extends Pessoa{
@@ -10,44 +11,77 @@ public class Aluno extends Pessoa{
 	private List<Pagamento> pagamentos; //	Relação de Composição  1:N Aluno - Pagamento 
     private boolean ativo; //falso caso aluno tiver com debitos;
 	
+    public Aluno(int matricula, String objetivo, String observacao, Treinador treinador, List<Pagamento> pagamentos,
+			boolean ativo) {
+		super();
+		this.matricula = matricula;
+		this.objetivo = objetivo;
+		this.observacao = observacao;
+		this.treinador = treinador;
+		this.pagamentos = pagamentos;
+		this.ativo = ativo;
+	}
+
+	public Aluno(int matricula, String objetivo, List<Pagamento> pagamentos, boolean ativo) {
+		super();
+		this.matricula = matricula;
+		this.objetivo = objetivo;
+		this.pagamentos = pagamentos;
+		this.ativo = ativo;
+	}
+	
+	public Aluno() {
+		this.pagamentos = new ArrayList<Pagamento>();
+		this.ativo = true;
+	}
+
 	public int getMatricula() {
 		return matricula;
 	}
-	public void setMatricula(int matricula) {
-		this.matricula = matricula;
-	}
+
 	public String getObjetivo() {
 		return objetivo;
 	}
-	public void setObjetivo(String objetivo) {
-		this.objetivo = objetivo;
-	}
+
 	public String getObservacao() {
 		return observacao;
 	}
-	public void setObservacao(String observacao) {
-		this.observacao = observacao;
-	}
+
 	public Treinador getTreinador() {
 		return treinador;
 	}
-	public void setTreinador(Treinador treinador) {
-		this.treinador = treinador;
-	}
+
 	public List<Pagamento> getPagamentos() {
 		return pagamentos;
 	}
-	public void setPagamentos(List<Pagamento> pagamentos) {
-		this.pagamentos = pagamentos;
-	}
-    public void adicionarPagamento(Pagamento pagamento) {
-        this.pagamentos.add(pagamento);
-    }
+
 	public boolean isAtivo() {
 		return ativo;
+	}
+
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+
+	public void setObjetivo(String objetivo) {
+		this.objetivo = objetivo;
+	}
+
+	public void setObservacao(String observacao) {
+		this.observacao = observacao;
+	}
+
+	public void setTreinador(Treinador treinador) {
+		this.treinador = treinador;
+	}
+
+	public void setPagamentos(List<Pagamento> pagamentos) {
+		this.pagamentos = pagamentos;
 	}
 
 	public void setAtivo(boolean ativo) {
 		this.ativo = ativo;
 	}
+	
+    
 }
